963d8dac92c1fe46203593b8c9e7576472ef2450
purnimasthrestha <sthapurnima90@gmail.com>
2026-08-12 12:40:33 +0545

    Add categories API URLs

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
     path("api/content/", include("content.urls")),
+    path("api/Categories/", include("Categories.urls")),
 
 ]   
