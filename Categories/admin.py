69500a4bc9029433ba709f58c5d1b74fd5cae61b
jessicathapa07 <jessicathapa497@gmail.com>
2026-08-13 11:35:16 +0545

    added new files

--- Categories/admin.py
+++ Categories/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category
 
 # Register your models here.
+@admin.register(Category)
+class PostCategory(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "description"
+    )
+
+    search_fields = (
+        "name",
+        "description",
+    )
